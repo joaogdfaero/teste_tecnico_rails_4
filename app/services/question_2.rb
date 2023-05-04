589bf2e7557ee1a4a4341900f3d0d7ad6172d9b1
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-05-04 08:31:15 -0300

    feat: remove diamantes e areia

--- app/services/question_2.rb
+++ app/services/question_2.rb
@@ -8,32 +8,38 @@ class TesteTecnico4
     diamond_count = 0
     areia_count  = 0
 
-    puts str
+     while str.include?("<>")
+      areia_count, diamond_count = count_occurences(areia_count, diamond_count, str)
+      remove_strings(str)
+     end
 
+    return "Diamantes extraídos = #{diamond_count}, Areias extraídas = #{areia_count}"
+  end
+
+  private
+
+  def remove_strings(str)
+    str.gsub!(".", "")
+    str.gsub!("<>", "")
+  end
+
+  def count_occurences(areia_count, diamond_count, str)
     str.each_char.with_index do |char, index|
       if char == '.'
         areia_count = areia_count + 1
       end
 
       if index != str.length - 1
-        puts str[index] << str[index + 1] #provavelmente o erro é pq ele tenta pegar o depois do último
+        if (str[index] << str[index + 1]) == '<>'
+          diamond_count = diamond_count + 1
+        end
       end
 
-
-      #if (str[index] + str[index + 1])  == '<>'
-      #  diamond_count = diamond_count + 1
-      #end
     end
-
-    str.gsub!(".","")
-    str.gsub("<>","")
-
-    puts areia_count
-    puts diamond_count
-    puts str
+    return areia_count, diamond_count
   end
-
 end
 
-input = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
-teste = TesteTecnico4.new(input)
+#input = '<<.<<..>><>><.>.>.<<.>.<.>>>><>><>>'
+#teste = TesteTecnico4.new(input)
+
