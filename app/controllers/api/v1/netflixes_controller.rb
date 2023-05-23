51116e87240e67a3691591164a0158f440d07c5c
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-05-23 06:54:42 -0300

    feat: controller basic REST actions

---
+++ app/controllers/api/v1/netflixes_controller.rb
@@ -0,0 +1,53 @@
+require 'csv'
+
+module Api
+    module v1
+        class NetflixesController < ApplicationController
+
+            # GET /netflixes
+            def index
+                @netflixes = Netflix.all
+
+                render json: @netflixes
+            end
+            
+            # GET /netflixes/1
+            def show
+                @netflix = Netflix.find(params[:id])
+                render json: {status: 'SUCCESS', message: 'Loaded Netflix Title', data:@netflix}, status: :ok  
+            end
+
+            # POST /netflixes
+            def create
+              @netflix = Netflix.new(netflix_params)
+              
+              if @netflix.save
+                render json: {status: 'SUCCESS', message: 'Saved Netflix', data:@netflix}, stauts: :ok
+              else
+                render json: {status: 'ERROR', message: 'Netflix not saved', data:@netflix.errors}, status: :unprocessable_entity
+              end
+            end
+
+            # PATCH/PUT /netflixes/1
+            def update
+                if @netflix.update(netflix_params)
+                    render json: {status: 'SUCCESS', message: 'Updated Netflix', data:@netflix}, stauts: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Netflix not updated', data:@netflix.errors}, status: :unprocessable_entity
+            end
+
+            # FALTA O DELETE, E O IMPORT_CSV
+
+
+
+            private
+            def netflix_params
+                params.require(:netflix).permit(:id_csv, :genre, :title, :director, :cast, :country,
+                                                :published_at, :year, :rating, :duration, :listed_in, :description)
+            end
+
+        
+            
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
- namespace :api do
-  namespace :v1 do
-    resources :netflixes
-    root to: 'netflix#index'
+  namespace :api do
+    namespace :v1 do
+      resources :netflixes
+      post 'titles/import_csv', to: 'titles#import_csv'
+      get 'testing', to: 'titles#method_test'
+      post 'imp_csv', to: 'titles#import_csv'
+    end
+  end
 end
+
+
+
