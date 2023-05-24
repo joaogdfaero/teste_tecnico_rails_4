ecbc7acd87e3bb265a8a74a48f89fc37d8e91d07
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-05-24 06:50:41 -0300

    feat: import csv action controller

--- app/controllers/api/v1/netflixes_controller.rb
+++ app/controllers/api/v1/netflixes_controller.rb
@@ -44,10 +44,27 @@ module Api
                 render json: {status: 'SUCCESS', message: 'Deleted Netflix', data:@netflix}, stauts: :ok
             end
 
-            # FALTA O IMPORTAR CSV
+            # IMPORT CSV
+            def import_csv
+                errors = []
 
+                CSV.foreach(params[:csv].path, headers: true) do |row|
+                    begin
+                        Netflix.create( show_id: row[0], type_title: row[1], title: row[2], director: row[3], cast: row[4],
+                      country: row[5], date_added: row[6], release_year: row[7], rating: row[8], duration: row[9],
+                      listed_in: row[10], description: row[11])
+                    
+                    rescue Exception => errors
+                        errors << err.message
+                    end
 
-
+                if errors.blank?
+                    render json: { first_message: "Arquivo CSV importado com sucesso!" }, status: :ok
+                else
+                    render json: { first_message: errors.join(', ') }, status: :unprocessable_entity
+                end
+                end
+            end
 
             private
             # Use callbacks to share common setup or constraints between actions.
@@ -59,10 +76,7 @@ module Api
             def netflix_params
                 params.require(:netflix).permit(:id_csv, :genre, :title, :director, :cast, :country,
                                                 :published_at, :year, :rating, :duration, :listed_in, :description)
-            end
-
-        
-            
+            end 
         end
     end
 end
